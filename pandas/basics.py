b454b4a22b6d39063fcb7a691ec20a98e9ec0281
aryanarora07 <aroraaryan824@gmail.com>
2025-02-01 04:01:51 -0800

    pandas ✅

---
+++ pandas/basics.py
@@ -0,0 +1,71 @@
+# Pandas is a Python library.
+
+# Pandas is used to analyze data.
+
+import pandas as pd
+
+
+# basic
+# mydataset = {
+#   'cars': ["BMW", "Volvo", "Ford"],
+#   'passings': [3, 7, 2]
+# }
+
+# myvar = pd.DataFrame(mydataset)
+
+# print(myvar)
+
+
+
+# Series is like a column in a table.
+
+# a = [1, 7, 2]
+
+# myvar = pd.Series(a)
+
+# print(myvar)
+
+
+
+# a = [1, 7, 2]
+
+# myvar = pd.Series(a, index = ["x", "y", "z"])
+
+# print(myvar)
+
+
+# calories = {"day1": 420, "day2": 380, "day3": 390}
+
+# myvar = pd.Series(calories, index = ["day1", "day2"])
+
+# print(myvar)
+
+
+
+
+#--------------------------------
+
+#dataframe is like a table.
+
+# data = {
+#     "calories": [420, 380, 390],
+#     "duration": [50, 40, 45]
+# }
+
+
+# myvar = pd.DataFrame(data)
+# print(myvar)
+
+# print(myvar.loc[0])
+
+
+#--------------------------------
+
+#read csv file
+
+# df = pd.read_csv('industry_sic.csv')
+# print(df.to_string())
+
+
+#--------------------------------
+
